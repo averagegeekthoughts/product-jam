1f064ff843f209d5bbbd7bf044d2b793f371f16c
makarenko <mariia.makarenko1@mail.huji.ac.il>
2022-11-04 20:33:25 +0200

    ex1

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-contactButton = document.querySelector("#contact button");
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
+button1.addEventListener("click", function changeColor(el) {
+  document.body.style.backgroundColor = 'salmon';
 });
+button2.addEventListener("click", function showHideText(el) {
+  const textToHide = document.getElementById('header');
+  if (textToHide.style.display == "block") {
+    textToHide.style.display = "none";
+  } else {
+    textToHide.style.display = "block";
+  }
+})
