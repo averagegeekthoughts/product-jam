3b7efd2bb514947ce3c020c16f7cf23787173eac
makarenko <mariia.makarenko1@mail.huji.ac.il>
2022-11-06 08:03:18 +0200

    ex1

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const button1 = document.getElementById('button1');
+var flag1 = true;
+
 const button2 = document.getElementById('button2');
+var flag2 = true;
 
 button1.addEventListener("click", function changeColor(el) {
-  document.body.style.backgroundColor = 'salmon';
-  document.body.style.color = 'white';
+  if (flag1) {
+    document.body.style.backgroundColor = 'salmon';
+    document.body.style.color = 'white';
+    flag1 = false;
+    return;
+  }
+  document.body.style.backgroundColor = '#EADEBA';
+  document.body.style.color = '#2E475D';
+  flag1 = true;
 });
+
 button2.addEventListener("click", function showHideText(el) {
   const textToHide = document.getElementById('header');
-  if (textToHide.style.display == "block") {
+  if (flag2) {
     textToHide.style.display = "none";
-  } else {
-    textToHide.style.display = "block";
-  }
+    flag2 = false;
+    return;
+  } 
+  textToHide.style.display = "block";
+  flag2 = true;
 })
